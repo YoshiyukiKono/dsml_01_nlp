419f3870077519c3920d0c910c1c8b0c997444ab
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2020-01-31 05:27:26 +0000

    Modified and add files.

---
+++ git_amend.sh
@@ -0,0 +1,4 @@
+git filter-branch -f --env-filter GIT_AUTHOR_NAME=YoshiyukiKono HEAD
+git filter-branch -f --env-filter GIT_COMMITTER_NAME=YoshiyukiKono HEAD
+git filter-branch -f --env-filter GIT_AUTHOR_EMAIL=yoshiyuki.kono@gmail.com HEAD
+git filter-branch -f --env-filter GIT_COMMITTER_NAME=yoshiyuki.kono@gmail.com HEAD
